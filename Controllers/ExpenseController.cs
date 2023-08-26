5ba39d613e8e8e12a554754fe832161b1aad3d36
MdReyadHossain <reyadhosen@gmail.com>
2023-08-27 04:08:37 +0600

    Expense Done and some bug fixed

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -74,9 +74,63 @@ namespace PerkyRabbit.Controllers
                                 where c.Id == catagoryEdit.Id
                                 select c).SingleOrDefault();
 
-            catagory.Name = catagoryEdit.Name;
+            var catagories = (from c in db.Catagories
+                              where c.Id != catagoryEdit.Id
+                              select c).ToList();
+
+            bool isValid = false;
+            foreach (var i in catagories)
+            {
+                if (i.Name.ToLower() == catagoryEdit.Name.ToLower())
+                {
+                    isValid = true;
+                }
+            }
+            if (isValid)
+            {
+                TempData["ErrMsg"] = "This Catagory already exist!";
+                return RedirectToAction("EditCatagory", "Expense", catagoryEdit.Id);
+            }
+            else
+            {
+                catagory.Name = catagoryEdit.Name;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Expense");
+            }
+        }
+
+        public ActionResult DeletedCatagory()
+        {
+            var db = new PerkyContext();
+            var catagories = (from catagory in db.Catagories
+                            where catagory.Status.Equals(false)
+                            select catagory).ToList();
+
+            return View(catagories);
+        }
+
+        public ActionResult DeleteCatagory(int id)
+        {
+            var db = new PerkyContext();
+            var catagory = (from c in db.Catagories
+                              where c.Id == id
+                              select c).SingleOrDefault();
+
+            catagory.Status = false;
             db.SaveChanges();
             return RedirectToAction("Index", "Expense");
+        }
+
+        public ActionResult RecycleCatagory(int id)
+        {
+            var db = new PerkyContext();
+            var catagory = (from c in db.Catagories
+                            where c.Id == id
+                            select c).SingleOrDefault();
+
+            catagory.Status = true;
+            db.SaveChanges();
+            return RedirectToAction("DeletedCatagory", "Expense");
         }
 
         public ActionResult Bills(int id)
@@ -87,12 +141,78 @@ namespace PerkyRabbit.Controllers
                             select c.Name).SingleOrDefault();
 
             ViewBag.Bill = catagory;
+            ViewBag.BillId = id;
             
             var bills = (from expense in db.Expenses
                          where expense.Catagory_Id == id
                          select expense).ToList();
 
             return View(bills);
+        }
+
+        [HttpGet]
+        public ActionResult AddBill(int id, string name)
+        {
+            ViewBag.Id = id;
+            ViewBag.Name = name;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddBill(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new PerkyContext();
+
+                db.Expenses.Add(expense);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Expense");
+            }
+            return View(expense);
+        }
+
+        [HttpGet]
+        public ActionResult EditBill(int id, string name, int catagoryId)
+        {
+            ViewBag.Name = name;
+            var db = new PerkyContext();
+            var expense = (from exp in db.Expenses
+                           where exp.Id == id
+                           select exp).SingleOrDefault();
+            return View(expense);
+        }
+
+        [HttpPost]
+        public ActionResult EditBill(Expense expenseEdit)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new PerkyContext();
+                var expense = (from exp in db.Expenses
+                                where exp.Id == expenseEdit.Id
+                                select exp).SingleOrDefault();
+
+                expense.FirstDate = expenseEdit.FirstDate;
+                expense.EndDate = expenseEdit.EndDate;
+                expense.Expenditure = expenseEdit.Expenditure;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Expense");
+
+            }
+            return View(expenseEdit);
+        }
+
+        public ActionResult DeleteBill(int id)
+        {
+            var db = new PerkyContext();
+            var expense = (from exp in db.Expenses
+                           where exp.Id == id
+                           select exp).SingleOrDefault();
+            db.Expenses.Remove(expense);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Expense");
         }
     }
 }
